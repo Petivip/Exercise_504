78b6e9676c06902ece716fc0e8dec51f0b9423c8
petivip <petivip@gmail.com>
2019-10-10 11:11:28 -0400

    504

---
+++ src/main/java/com/example/demo/Exercise504Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Exercise504Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Exercise504Application.class, args);
+    }
+
+}
